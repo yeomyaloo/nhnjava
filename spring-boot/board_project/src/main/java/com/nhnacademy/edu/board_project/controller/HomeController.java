c9a60db8abb23b016ff383e028a70a6602fa06a1
hwiju yeom <81970382+yeomyaloo@users.noreply.github.com>
2022-12-14 18:26:15 +0900

    12./14

---
+++ spring-boot/board_project/src/main/java/com/nhnacademy/edu/board_project/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.edu.board_project.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
+}
---
+++ spring-boot/student/src/test/java/com/nhnacademy/edu/springboot/student/controller/StudentControllerTest.java
@@ -0,0 +1,20 @@
+package com.nhnacademy.edu.springboot.student.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void getStudents() {
+    }
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void createStudent() {
+    }
+}
